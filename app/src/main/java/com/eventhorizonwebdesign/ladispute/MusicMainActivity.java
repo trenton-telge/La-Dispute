d4bd2d9cb4657a45a9484177869e636e9bfc9254
Trenton <darkforce102@gmail.com>
2015-11-21 15:55:17 -0600

    Added onGlobalLayout inner classes to MainMusicActivity.class

--- app/src/main/java/com/eventhorizonwebdesign/ladispute/MusicMainActivity.java
+++ app/src/main/java/com/eventhorizonwebdesign/ladispute/MusicMainActivity.java
@@ -2,11 +2,10 @@ package com.eventhorizonwebdesign.ladispute;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Display;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 
 public class MusicMainActivity extends AppCompatActivity {
@@ -18,28 +17,50 @@ public class MusicMainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int viewport_width = size.x;
-        int viewport_height = size.y;//unused as of now
-        int albumdim = (int)((double)viewport_width-32-8-8-8)/2;
-        ImageButton vancouverAlbum = (ImageButton)findViewById(R.id.vancouverAlbum);
-        Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.vancouver);
-        Bitmap resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
-        vancouverAlbum.setImageBitmap(resizedAlbumBitmap);
-        ImageButton satbotrbvaaAlbum = (ImageButton)findViewById(R.id.satbotrbvaaAlbum);
-        albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.satbotrbvaa);
-        resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
-        satbotrbvaaAlbum.setImageBitmap(resizedAlbumBitmap);
-        ImageButton wildlifeAlbum = (ImageButton)findViewById(R.id.wildlifeAlbum);
-        albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wildlife);
-        resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
-        wildlifeAlbum.setImageBitmap(resizedAlbumBitmap);
-        ImageButton roomsAlbum = (ImageButton)findViewById(R.id.roomsAlbum);
-        albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rooms);
-        resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
-        roomsAlbum.setImageBitmap(resizedAlbumBitmap);
+        final ImageButton vancouverAlbum = (ImageButton)findViewById(R.id.vancouverAlbum);
+        ViewTreeObserver observer = vancouverAlbum.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int albumdim = vancouverAlbum.getWidth()-8;
+                Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.vancouver);
+                Bitmap resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
+                vancouverAlbum.setImageBitmap(resizedAlbumBitmap);
+            }
+        });
+        final ImageButton satbotrbvaaAlbum = (ImageButton)findViewById(R.id.satbotrbvaaAlbum);
+        observer = satbotrbvaaAlbum.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int albumdim = satbotrbvaaAlbum.getWidth()-8;
+                Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.satbotrbvaa);
+                Bitmap resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
+                satbotrbvaaAlbum.setImageBitmap(resizedAlbumBitmap);
+            }
+        });
+        final ImageButton wildlifeAlbum = (ImageButton)findViewById(R.id.wildlifeAlbum);
+        observer = wildlifeAlbum.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int albumdim = wildlifeAlbum.getWidth()-8;
+                Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wildlife);
+                Bitmap resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
+                wildlifeAlbum.setImageBitmap(resizedAlbumBitmap);
+            }
+        });
+        final ImageButton roomsAlbum = (ImageButton)findViewById(R.id.roomsAlbum);
+        observer = roomsAlbum.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int albumdim = roomsAlbum.getWidth()-8;
+                Bitmap albumBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rooms);
+                Bitmap resizedAlbumBitmap = Bitmap.createScaledBitmap(albumBitmap, albumdim, albumdim, true);
+                roomsAlbum.setImageBitmap(resizedAlbumBitmap);
+            }
+        });
     }
 
 }
